0406416d6b69dacf0a826ae972d33394507df2ca
Boniface Okonkwo <63077536+Temple712@users.noreply.github.com>
2025-09-21 12:04:36 +0200

    Update SQProject.sql

--- SQProject.sql
+++ SQProject.sql
@@ -138,3 +138,35 @@ ON c.address_id=a.address_id
 GROUP BY district, c.customer_id) sub
 GROUP BY district
 ORDER BY 2 DESC
+
+-- Task: Create a list that shows all payments including the payment_id, amount, and the film category (name) plus the total amount that was 
+-- made in this category. Order the results ascendingly by the category (name) and as second order criterion by the payment_id ascendingly.
+SELECT
+title,
+amount,
+name,
+payment_id,
+(SELECT SUM(amount) FROM payment p
+LEFT JOIN rental r
+ON r.rental_id=p.rental_id
+LEFT JOIN inventory i
+ON i.inventory_id=r.inventory_id
+LEFT JOIN film f
+ON f.film_id=i.film_id
+LEFT JOIN film_category fc
+ON fc.film_id=f.film_id
+LEFT JOIN category c1
+ON c1.category_id=fc.category_id
+WHERE c1.name=c.name)
+FROM payment p
+LEFT JOIN rental r
+ON r.rental_id=p.rental_id
+LEFT JOIN inventory i
+ON i.inventory_id=r.inventory_id
+LEFT JOIN film f
+ON f.film_id=i.film_id
+LEFT JOIN film_category fc
+ON fc.film_id=f.film_id
+LEFT JOIN category c
+ON c.category_id=fc.category_id
+ORDER BY name
